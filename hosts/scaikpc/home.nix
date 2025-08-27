be087102681cfb1c5f4fda9eed065ced6a2cff87
Evsey Kazansky <67639168+scaik@users.noreply.github.com>
2025-08-27 17:06:43 +0300

    ♻️ refactor: scaikpc home

--- hosts/scaikpc/home.nix
+++ hosts/scaikpc/home.nix
@@ -10,11 +10,18 @@
     obsidian
     yandex-disk
     claude-code
+    vscode
   ];
 
   hyprland.configText = ''
     monitor = DP-2,1920x1080@144,0x0,1
     monitor = HDMI-A-1,1920x1080@100,-1920x220,1
+  '';
+
+  programs.ssh.extraConfig = ''
+    Host 193.106.172.133 snab.usenx.ru aitrener.usenx.ru d.usenx.ru
+        AddKeysToAgent yes
+        IdentityFile ~/.ssh/neurox_dedicated
   '';
 
 }
